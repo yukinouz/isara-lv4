530ff4468e2cc68105befe7ceb5174c246d0c5b0
野瀬友岐 <karl.vanc0uver1st@gmail.com>
2019-10-28 02:50:19 +0900

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,80 @@
+
+// WordPress対応のため"jQuery"で記述開始＋function($)で以降は$の使用を可能にする
+jQuery(function($){
+  // ページリロードボタン
+  $('#reload').on('click', function(){
+		location.reload();
+  });
+
+// ページ内リンクを全てスムーススクロールにする
+	$('a[href^="#"]').on('click',function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+  // ページトップボタン
+  // 最初は隠す
+  $('#pagetop').hide();
+  // 100スクロールしたらボタンが出現、それ以外は隠す
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#pagetop').fadeIn();
+    } else {
+      $('#pagetop').fadeOut();
+    }
+  });
+  // クリックでトップへ戻る
+  // animate.cssで調整
+  $('#pagetop').click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+  // カレントページナビゲーション
+  $(function () {
+    $('#gNav li a').each(function () {
+      // 各aタグのhref内の値を取得する
+      var $href = $(this).attr('href');
+      if (location.href.match($href)) {
+        $(this).addClass('current');
+        // liを取りたい場合
+        // $(this).parent('li').addClass('current');
+      } else {
+        $(this).removeClass('current');
+        // liを取りたい場合
+        // $(this).parent('li').removeClass('current');
+      }
+    });
+  });
+
+  // fadeInアニメーションを追加
+  // animate.cssで調整
+  $(window).scroll(function (){
+    $('.fade-in').each(function(){
+        var ptop = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > ptop - windowHeight + 100){
+            $(this).addClass('scroll-in');
+        }
+    });
+  });
+  var scroll = $(window).scrollTop();
+  if (scroll >= 1){
+  $(window).ready(function (){
+  var scroll = $(window).scrollTop();
+
+  $('.fade-in').addClass('scroll-in');
+  });
+  }
+
+  $('.drawer-nav a').on('click', function () {
+    $('#drawer-checkbox').prop('checked', false);
+  });
+});
